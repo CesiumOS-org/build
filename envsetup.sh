211a52d4d2bbe0ff3dd9ae9c41324c1db4d9790d
BunsExynos <himanshuakela@gmail.com>
2019-12-05 14:06:03 +0000

    Get ready for CesiumOS!
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -142,8 +142,8 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
-    if (echo -n $1 | grep -q -e "^aosp_") ; then
-        CUSTOM_BUILD=$(echo -n $1 | sed -e 's/^aosp_//g')
+    if (echo -n $1 | grep -q -e "^cesium_") ; then
+        CUSTOM_BUILD=$(echo -n $1 | sed -e 's/^cesium_//g')
         export BUILD_NUMBER=$( (date +%s%N ; echo $CUSTOM_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10 )
     else
         CUSTOM_BUILD=
--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -906,20 +906,37 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   android_version = target_info.GetBuildProp("ro.build.version.release")
   build_id = target_info.GetBuildProp("ro.build.id")
-  build_date = target_info.GetBuildProp("org.pixelexperience.build_date")
+  build_date = target_info.GetBuildProp("org.cesium.build_date")
   security_patch = target_info.GetBuildProp("ro.build.version.security_patch")
-  device = target_info.GetBuildProp("org.pixelexperience.device")
+  device = target_info.GetBuildProp("org.cesium.device")
+  manufacturer = target_info.GetBuildProp("ro.product.manufacturer")    
+  buildtype = target_info.GetBuildProp("org.cesium.build_type")
+  buildhst = target_info.GetBuildProp("ro.build.host")
+  maintainer = target_info.GetBuildProp("ro.build.user")
+  sdkver = target_info.GetBuildProp("ro.build.version.sdk")
 
-  script.Print("----------------------------------------------");
-  script.Print("              Pixel Experience");
-  script.Print("               by jhenrique09");
-  script.Print("----------------------------------------------");
-  script.Print(" Android version: %s"%(android_version));
-  script.Print(" Build id: %s"%(build_id));
-  script.Print(" Build date: %s"%(build_date));
-  script.Print(" Security patch: %s"%(security_patch));
-  script.Print(" Device: %s"%(device));
-  script.Print("----------------------------------------------");
+  script.Print("                                                                 ")
+  script.Print("                                                                 ")
+  script.Print("_________               .__                                      ")
+  script.Print("\_   ___ \  ____   _____|__|__ __  _____                         ")
+  script.Print("/    \  \/_/ __ \ /  ___/  |  |  \/     \                        ")
+  script.Print("\     \___\  ___/ \___ \|  |  |  /  Y Y  \                       ")
+  script.Print(" \______  /\___  >____  >__|____/|__|_|  /                       ")
+  script.Print("       \/     \/     \/               \/                         ")
+  script.Print("                                                                 ")
+  script.Print("                                                                 ")
+  script.Print(" ############### INFO ###############             "               )
+  script.Print(" Build id: %s"%(build_id)                                         )
+  script.Print(" Android version: %s"%(android_version)                           )
+  script.Print(" Security patch: %s"%(security_patch)                             )
+  script.Print(" SdK ver: %s"%(sdkver)                                            )
+  script.Print(" Build date: %s"%(build_date)                                     )
+  script.Print(" Build Type: %s"%(buildtype)                                      )
+  script.Print(" Build Host: %s"%(buildhst)                                       )
+  script.Print(" Maintainer: %s"%(maintainer)                                     )
+  script.Print(" Device: %s"%(device)                                             )
+  script.Print(" Manufacturer: %s"%(manufacturer)                                 )
+  script.Print(" ####################################             "               )
   device_specific.FullOTA_InstallBegin()
 
   system_progress = 0.75
