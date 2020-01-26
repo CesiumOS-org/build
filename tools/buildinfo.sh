LineageOS/android_build

Allow overriding platform SPL
 * Google started verifying platform SPL as part of SafetyNet ctsProfile
   test, overriding build fingerprint and description is no longer
   enough.
 * Introduce a new PLATFORM_SECURITY_PATCH_OVERRIDE variable to allow
   overriding platform SPL while keeping the real SPL in a custom prop,
   so that the settings app could show the real one to avoid confusion.

Change-Id: I58f4affc826e79b4a1a05af45ad0a116d4846a72

tools/buildinfo.sh
@@ -12,7 +12,12 @@ echo "ro.build.version.preview_sdk_fingerprint=$PLATFORM_PREVIEW_SDK_FINGERPRINT
 echo "ro.build.version.codename=$PLATFORM_VERSION_CODENAME"
 echo "ro.build.version.all_codenames=$PLATFORM_VERSION_ALL_CODENAMES"
 echo "ro.build.version.release=$PLATFORM_VERSION"
-echo "ro.build.version.security_patch=$PLATFORM_SECURITY_PATCH"
+if [ -n "$PLATFORM_SECURITY_PATCH_OVERRIDE" ] ; then
+  echo "ro.build.version.security_patch=$PLATFORM_SECURITY_PATCH_OVERRIDE"
+  echo "ro.lineage.build.version.security_patch=$PLATFORM_SECURITY_PATCH"
+else
+  echo "ro.build.version.security_patch=$PLATFORM_SECURITY_PATCH"
+fi
 echo "ro.build.version.base_os=$PLATFORM_BASE_OS"
 echo "ro.build.version.min_supported_target_sdk=$PLATFORM_MIN_SUPPORTED_TARGET_SDK_VERSION"
 echo "ro.build.date=`$DATE`"

CesiumOS-org/build

tools/buildinfo.sh
@@ -12,7 +12,12 @@ echo "ro.build.version.preview_sdk_fingerprint=$PLATFORM_PREVIEW_SDK_FINGERPRINT
 echo "ro.build.version.codename=$PLATFORM_VERSION_CODENAME"
 echo "ro.build.version.all_codenames=$PLATFORM_VERSION_ALL_CODENAMES"
 echo "ro.build.version.release=$PLATFORM_VERSION"
-echo "ro.build.version.security_patch=$PLATFORM_SECURITY_PATCH"
+if [ -n "$PLATFORM_SECURITY_PATCH_OVERRIDE" ] ; then
+  echo "ro.build.version.security_patch=$PLATFORM_SECURITY_PATCH_OVERRIDE"
+  echo "ro.evolution.build.version.security_patch=$PLATFORM_SECURITY_PATCH"
+else
+  echo "ro.build.version.security_patch=$PLATFORM_SECURITY_PATCH"
+fi
 echo "ro.build.version.base_os=$PLATFORM_BASE_OS"
 echo "ro.build.version.min_supported_target_sdk=$PLATFORM_MIN_SUPPORTED_TARGET_SDK_VERSION"
 echo "ro.build.date=`$DATE`"