624aabdc0914c31a13d315e95b018859fbb9f4a7
SahilSonar <sss.sonar2003@gmail.com>
2020-01-26 13:44:51 +0000

    build: Use ro.build.date instead
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -906,7 +906,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   android_version = target_info.GetBuildProp("ro.build.version.release")
   build_id = target_info.GetBuildProp("ro.build.id")
-  build_date = target_info.GetBuildProp("org.cesium.build_date")
+  build_date = target_info.GetBuildProp("ro.build.date")
   security_patch = target_info.GetBuildProp("ro.build.version.security_patch")
   device = target_info.GetBuildProp("org.cesium.device")
   manufacturer = target_info.GetBuildProp("ro.product.manufacturer")    
